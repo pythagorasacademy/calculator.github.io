74c5e44d6ceb01d18b5b25998ff28fb93230e866
PythagorasAcademy <s.s.programming.edu@gmail.com>
2021-02-06 22:50:51 +0100

    whole project

---
+++ მარიამ-რატი-გაბრიელ/fizika.js
@@ -0,0 +1,118 @@
+function st(){
+    var ricxvi1 = parseInt(document.getElementById('ricxvi1').value);
+    var pasuxi = ricxvi1 * 60;
+    document.getElementById('pasuxi1').innerHTML = 'ეს საათი წუთებში = ' + pasuxi;
+              }   
+function wt(){                
+    var ricxvi2 = parseInt(document.getElementById('ricxvi2').value);
+    var pasux = ricxvi2 * 60;
+    document.getElementById('pasux').innerHTML = 'ეს წუთები წამებში = ' + pasux;
+              }        
+function dge(){
+    var number2= parseInt(document.getElementById('number2').value);
+    var pasu = number2 * 24;
+    document.getElementById('pasu').innerHTML = 'ეს დღე საათებში = ' + pasu;
+              }        
+function dgewt(){
+    var number3 = parseInt(document.getElementById('number3').value);
+    var pas = number3 * 1440;
+    document.getElementById('pas').innerHTML = 'ეს დღე წუთებში = ' + pas;
+          }
+function dgewm(){
+    var number4 = parseInt(document.getElementById('number4').value);
+    var pa = number4 * 86400;
+    document.getElementById('pasuxe4').innerHTML = 'ეს დღე წამებში = ' + pa;
+          }
+function kvdg(){
+    var ricxvi6 = parseInt(document.getElementById('ricxvi6').value);
+    var shedegi5 = ricxvi6 * 7;
+    document.getElementById('pasuxe5').innerHTML = 'ეს კვირა დღეებში = ' + shedegi5;
+          } 
+function kvirasaatebshi(){
+    var ricxvi5 = parseInt(document.getElementById('ricxvi5').value);
+    var shedegi6 = ricxvi5 * 168;
+    document.getElementById('pasuxe6').innerHTML = 'ეს კვირა საათებში = ' + shedegi6;
+          } 
+function kvirawutebshi(){
+    var ricxvi4 = parseInt(document.getElementById('ricxvi4').value);
+    var shedegi7 = ricxvi4 * 10080;
+    document.getElementById('pasuxebi').innerHTML = 'ეს კვირა წუთებში = ' + shedegi7;
+          }
+function kvirawamebshi(){
+    var ricxvi15 = parseInt(document.getElementById('ricxvi15').value);
+    var shedegi8 = ricxvi15 * 604800;
+    document.getElementById('pasuxeb').innerHTML = 'ეს კვირა წამებში = ' + shedegi8;
+          }
+function tonacentnershi(){
+    var tona = parseInt(document.getElementById('tona').value);
+    var shede = tona * 100000;
+    document.getElementById('shedegi').innerHTML = 'ეს ტონა ცენტნერში = ' + shede;
+          }  
+function centnerikilogramshi(){
+    var kilogrami = parseInt(document.getElementById('kilogrami').value);
+    var shed = kilogrami * 100;
+    document.getElementById('shedeg').innerHTML = 'ეს ცენტნერი კილოგრამებში = ' + shed;
+          }  
+function kilogramigramshi(){
+    var grami = parseInt(document.getElementById('grami').value);
+    var shedi = grami * 1000;
+    document.getElementById('shedegi1').innerHTML = 'ეს კილოგრამები გრამებში = ' + shedi;
+          }  
+function gramimiligramshi(){
+    var miligrami = parseInt(document.getElementById('miligrami').value);
+    var shedo = miligrami * 1000;
+    document.getElementById('pasuxebo').innerHTML = 'ეს გრამი მილიგრამებში = ' + shedo;
+
+          }    
+          function fanqari(){
+            var ricxvi11 = parseInt(document.getElementById('pasta').value);
+            var shedegi1 = ricxvi11 * 1000;
+            document.getElementById('pasuxisVeli1').innerHTML = 'ეს მეტრი მილიმეტრებში= ' + shedegi1;
+            }
+            function ioio(){
+                var ricxvi2 = parseInt(document.getElementById('datunia').value);
+                var shedegi2 = ricxvi2 * 100;
+                document.getElementById('mopasuxe2').innerHTML = 'ეს მეტრი სანტიმეტრებში= ' + shedegi2;
+                }
+                function miwa(){
+                    var ricxvi3 = parseInt(document.getElementById('yvavili').value);
+                    var shedegi3 = ricxvi3 * 10;
+                    document.getElementById('pasuxe2').innerHTML = 'ეს მეტრი დეციმეტრებში= ' + shedegi3;
+                    }
+                    function rati(){
+                        var ricxvi4 = parseInt(document.getElementById('gabrieli').value);
+                        var shedegi4 = ricxvi4 * 1000000;
+                        document.getElementById('mpasuxe2').innerHTML = 'ეს კილომეტრი მილიმეტრებში= ' + shedegi4;
+                        }
+                        function fotoli(){
+                            var ricxvi5 = parseInt(document.getElementById('rveuli').value);
+                            var shedegi5 = ricxvi5 * 100000;
+                            document.getElementById('lmpasuxe2').innerHTML = 'ეს კილომეტრი სანტიმეტრებში= ' + shedegi5;
+                            }
+                            function tavi(){
+                                var ricxvi6 = parseInt(document.getElementById('keva').value);
+                                var shedegi6 = ricxvi6 * 10000;
+                                document.getElementById('mlmpasuxe2').innerHTML = 'ეს კილომეტრი დეციმეტრებში= ' + shedegi6;
+                                }
+                                function gili(){
+                                    var ricxvi7 = parseInt(document.getElementById('mausi').value);
+                                    var shedegi7 = ricxvi7 * 1000;
+                                    document.getElementById('lmlmpasuxe2').innerHTML = 'ეს კილომეტრი მეტრებში= ' + shedegi7;
+                                    }
+                                    function rva(){
+                                        var ricxvi8 = parseInt(document.getElementById('shvidi').value);
+                                        var shedegi8 = ricxvi8 * 10;
+                                        document.getElementById('mlmlmpasuxe2').innerHTML = 'ეს დეციმეტრები სანტიმეტრებში = ' + shedegi8;
+                                        }
+                                        function cxra(){
+                                            var ricxvi9 = parseInt(document.getElementById('moli').value);
+                                            var shedegi9 = ricxvi9 * 100;
+                                            document.getElementById('lmlmlmpasuxe2').innerHTML = 'ეს დეციმეტრები მილიმეტრებში = ' + shedegi9;
+                                            }
+                                            function orri(){
+                                                var ricxvi90 = parseInt(document.getElementById('noli').value);
+                                                var shedegi90 = ricxvi90 * 10;
+                                                document.getElementById('mlmlmlmpasuxe2').innerHTML = 'ეს სანტიმეტრები მილიმეტრებში = ' + shedegi90;
+                                                }
+   
+                                  
---
+++ ნიკოლოზ ბოლქვაძე/quiz.js
@@ -0,0 +1,28 @@
+function check() {
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    var q6=document.quiz.question6.value;
+    var q7=document.quiz.question7.value;
+    var q8=document.quiz.question8.value;
+    var result=document.getElementById('result')
+    var quiz=document.getElementById("quiz");
+    if (q1=="143") {c++}
+    if (q2=="20") {c++} 
+    if (q3=="ara") {c++}
+    if (q4=="625") {c++}
+    if (q5=="24") {c++}
+    if (q6=="25") {c++}
+    if (q7=="MCXXI") {c++}
+    if (q8=="59") {c++}
+   quiz.style.display="none"
+   
+   if (c<=4) {
+    result.textContent=`თქვენი შედეგია ${c}. არც ისე კარგია, გთხოვთ შეეცადოთ იმუშაოთ საკუთარ თავზე.`
+   } else {
+    result.textContent=`შენი შედეგია ${c}. გასაოცარია. შევინარჩუნოთ ის!.`
+   }
+}
